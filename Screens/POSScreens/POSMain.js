7adf567e1cff9730997643176191950730328bd1
NasirKhatri <45204768+NasirKhatri@users.noreply.github.com>
2022-07-27 19:53:27 +0500

    Worked on UI of POSMain page, work still in progress

--- Screens/POSScreens/POSMain.js
+++ Screens/POSScreens/POSMain.js
@@ -1,14 +1,76 @@
 import * as React from 'react';
-import { View, Text, Button } from 'react-native';
+import { View, Text, TouchableOpacity } from 'react-native';
+import { StyleSheet } from 'react-native';
+import { YellowBox } from 'react-native-web';
 
 import globalStyles from '../../globalStyles';
 
-const POSMainScreen = ({navigation}) => {
-    return(
-        <View style={globalStyles.body}>
-            <Text>This Is POS Main Screen</Text>
+
+
+const POSMainScreen = ({ navigation }) => {
+
+    const POSButton1 = ({ text, onPress }) => {
+        return (
+            <TouchableOpacity style={{ flex: 1 }} onPress={onPress}>
+                <View style={styles.CustomerTypeButton}>
+                    <Text style={styles.ButtonText}>{text}</Text>
+                </View>
+            </TouchableOpacity>
+        )
+    }
+
+    return (
+        <View style={{...globalStyles.body, paddingHorizontal: 4, paddingTop: 4}}>
+            <View style={{ flexDirection: 'row' }}>
+                <POSButton1 text='Customer 1' onPress={() => null} />
+                <POSButton1 text='Customer 2' onPress={() => null} />
+                <POSButton1 text='Customer 3' onPress={() => null} />
+            </View>
+            <View style={{ flexDirection: 'row' }}>
+                <POSButton1 text='Dine In' onPress={() => null} />
+                <POSButton1 text='Take Away' onPress={() => null} />
+                <POSButton1 text='Delivery' onPress={() => null} />
+            </View>
+            <View style={{ flex: 1, ...styles.Section }}>
+                <Text style={styles.title}>Categories</Text>
+            </View>
+            <View style={{ flex: 1.5, ...styles.Section }}>
+                <Text style={styles.title}>Items</Text>
+            </View>
+            <View style={{ flexDirection: 'row' }}>
+                <POSButton1 text='6 Items' onPress={() => null} />
+                <POSButton1 text='Rs 4000' onPress={() => null} />
+                <POSButton1 text='Details' onPress={() => navigation.navigate('POSDetail')} />
+            </View>
+            <View style={{ flexDirection: 'row' }}>
+                <POSButton1 text='Discard Sale' onPress={() => null} />
+                <POSButton1 text='Recall' onPress={() => null} />
+                <POSButton1 text='Pay' onPress={() => null} />
+            </View>
         </View>
     )
 }
+
+const styles = StyleSheet.create({
+    CustomerTypeButton: {
+        backgroundColor: 'lightblue',
+        marginHorizontal: 4,
+        padding: 6,
+        marginBottom: 6
+    },
+    ButtonText: {
+        fontWeight: 'bold',
+        textAlign: 'center',
+        fontSize: 18
+    },
+    Section: {
+ 
+    },
+    title: {
+        fontWeight: 'bold',
+        fontSize: 16,
+        letterSpacing: 1
+    }
+})
 
 export default POSMainScreen;
