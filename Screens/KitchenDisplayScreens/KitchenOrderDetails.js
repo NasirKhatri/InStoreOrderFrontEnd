d74256fe6f1d68ebc0c279c2a39fc83d0d960be6
NasirKhatri <45204768+NasirKhatri@users.noreply.github.com>
2022-08-02 21:23:46 +0500

    Library added for check box

--- Screens/KitchenDisplayScreens/KitchenOrderDetails.js
+++ Screens/KitchenDisplayScreens/KitchenOrderDetails.js
@@ -1,5 +1,6 @@
 import * as React from 'react';
 import { View, Text } from 'react-native';
+import BouncyCheckbox from "react-native-bouncy-checkbox";
 
 import KitchenOrder from '../../Components/KitchenOrder';
 
@@ -7,11 +8,18 @@ import globalStyles from '../../globalStyles';
 
 const DetailLine = () => {
     const [checked, setChecked] = React.useState(false);
-    return(
-        <View style={{flexDirection: 'row'}}>
-            <Text style={{flexBasis: '15%'}}>2</Text>
-            <Text style={{flexBasis: '75%'}}>Zinger Burger With Cheese</Text>
-        </View>
+    return (
+            <BouncyCheckbox
+                size={30}
+                fillColor="red"
+                unfillColor="#FFFFFF"
+                text="Custom Checkbox"
+                iconStyle={{ borderColor: "red" }}
+                iconInnerStyle={{ borderWidth: 4 }}
+                textStyle={{ fontFamily: "JosefinSans-Regular" }}
+                onPress={() => setChecked(!checked)}
+                style={{margin: 15}}
+            />
     )
 }
 
@@ -21,11 +29,9 @@ const KitchenOrderDetailsScreen = ({ navigation }) => {
         <View style={{ ...globalStyles.body, paddingHorizontal: 8 }}>
             <View>
                 <KitchenOrder item={order} navigation={navigation} />
-                <View style={{ flexDirection: 'row' }}>
-                    <Text style={{ flexBasis: '10%', textAlign: 'center', fontWeight: 'bold' }}> </Text>
-                    <Text style={{ flexBasis: '15%', textAlign: 'center', fontWeight: 'bold' }}>Qty</Text>
-                    <Text style={{ flexBasis: '75%', textAlign: 'center', fontWeight: 'bold' }}>Item</Text>
-                </View>
+                <DetailLine/>
+                <DetailLine/>
+                <DetailLine/>
             </View>
         </View>
     )
