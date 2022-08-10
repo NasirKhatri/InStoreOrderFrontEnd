70c425af02a7485927f456d440d4fb7955ce982f
NasirKhatri <45204768+NasirKhatri@users.noreply.github.com>
2022-08-10 21:22:02 +0500

    Validation added in Add Customer screen and Check box container component also updated to pass name of the checkbox

--- Components/CheckBoxContainer.js
+++ Components/CheckBoxContainer.js
@@ -4,13 +4,13 @@ import Checkbox from 'expo-checkbox'
 
 import globalStyles from '../globalStyles'
 
-export const CheckBoxContainer = ({value, setValue, text}) => {
+export const CheckBoxContainer = ({value, setValue, text, name}) => {
     return(
         <View style={{ flexDirection: 'row', marginTop: 15 }}>
         <Checkbox
             style={globalStyles.Checkbox}
             value={value}
-            onValueChange={setValue}
+            onValueChange={name ? nextValue => setValue(name, nextValue) : setValue}
             color={value ? globalStyles.CheckedColor.color : undefined}
         />
         <Text>{text}</Text>
--- Screens/SettingScreens/AddCustomer.js
+++ Screens/SettingScreens/AddCustomer.js
@@ -1,11 +1,42 @@
 import React from 'react'
-import { View, TouchableWithoutFeedback, TextInput, ScrollView, Keyboard } from 'react-native'
+import { View, TouchableWithoutFeedback, TextInput, Text, ScrollView, Keyboard } from 'react-native'
 
 import { FlatButton } from '../../Components/Button'
 import { Dropdown } from '../../Components/Dropdown';
 import { CheckBoxContainer } from '../../Components/CheckBoxContainer';
+import { KeyboardAwareScrollView } from 'react-native-keyboard-aware-scroll-view'
+
+import { Formik, ErrorMessage } from 'formik';
+import * as Yup from "yup";
 
 import globalStyles from '../../globalStyles'
+
+const initialValues = {
+    Name: '',
+    Gender: 'Male',
+    Address: '',
+    PhoneNo: '',
+    Email: '',
+    DOB: '',
+    CNIC: '',
+    CreditCustomer: false,
+}
+
+const CustomerSchema = Yup.object().shape({
+    Name: Yup.string().min(2, 'Too Short').max(20, 'Too Long').required('Required'),
+    Gender: Yup.string(),
+    Address: Yup.string().min(10, 'Too Short').max(60, 'Too Long'),
+    PhoneNo: Yup.string().matches(/[0-9]{4}-[0-9]{7}/, 'Invalid Phone No').required('Required'),
+    Email: Yup.string().email('Invalid Email').required('Required'),
+    DOB: Yup.string(),
+    CNIC: Yup.string().matches(/[0-9]{5}-[0-9]{7}-[0-9]{1}/, 'Invalid CNIC No'),
+})
+
+const addCustomerRequest = (values, actions) => {
+    console.log(values);
+    actions.resetForm();
+
+}
 
 export const AddCustomer = () => {
     const [isChecked, setChecked] = React.useState(false);
@@ -14,19 +45,55 @@ export const AddCustomer = () => {
     return (
         <TouchableWithoutFeedback onPress={() => Keyboard.dismiss()}>
             <View style={globalStyles.body}>
-                <View style={{ flex: 1, justifyContent: 'flex-start' }}>
-                    <ScrollView>
-                        <TextInput style={globalStyles.input} placeholder="Customer Name" />
-                        <Dropdown value={gender} setValue={setGender} data={['Male', 'Female']}/>
-                        <TextInput style={globalStyles.input} placeholder="Customer Address" />
-                        <TextInput keyboardType='numeric' style={globalStyles.input} placeholder="Phone Number (i.e. 0123-1234567)" />
-                        <TextInput style={globalStyles.input} placeholder="Customer Email" />
-                        <TextInput style={globalStyles.input} placeholder="Date of Birth" />
-                        <TextInput style={globalStyles.input} placeholder="Customer CNIC" />
-                        <CheckBoxContainer value={isChecked} setValue={setChecked} text="Credit Customer"/>
-                    </ScrollView>
-                </View>
-                <FlatButton text='Add Customer' />
+                <Formik initialValues={initialValues} validationSchema={CustomerSchema} onSubmit={(values, actions) => addCustomerRequest(values, actions)}>
+                    {({ handleChange, handleBlur, handleSubmit, setFieldValue, values, errors, touched }) => (
+                        <>
+                            <View style={{ flex: 1, justifyContent: 'flex-start' }}>
+                                <KeyboardAwareScrollView>
+                                    <TextInput
+                                        style={globalStyles.input} placeholder="Customer Name"
+                                        onChangeText={handleChange('Name')}
+                                        onBlur={handleBlur('Name')}
+                                        value={values.Name} />
+                                    {errors.Name && touched.Name ? <Text style={globalStyles.ErrorMessages}><ErrorMessage name='Name' /></Text> : <></>}
+                                    <Dropdown value={values.Gender} setValue={handleChange('Gender')} data={['Male', 'Female']} />
+                                    <TextInput 
+                                        style={globalStyles.input} placeholder="Customer Address"
+                                        onChangeText={handleChange('Address')}
+                                        onBlur={handleBlur('Address')}
+                                        value={values.Address} />
+                                    {errors.Address && touched.Address ? <Text style={globalStyles.ErrorMessages}><ErrorMessage name='Address' /></Text> : <></>}
+                                    <TextInput 
+                                        keyboardType='numeric' style={globalStyles.input} placeholder="Phone Number (i.e. 0123-1234567)"
+                                        onChangeText={handleChange('PhoneNo')}
+                                        onBlur={handleBlur('PhoneNo')}
+                                        value={values.PhoneNo} />
+                                    {errors.PhoneNo && touched.PhoneNo ? <Text style={globalStyles.ErrorMessages}><ErrorMessage name='PhoneNo' /></Text> : <></>}
+                                    <TextInput 
+                                        style={globalStyles.input} placeholder="Customer Email"
+                                        onChangeText={handleChange('Email')}
+                                        onBlur={handleBlur('Email')}
+                                        value={values.Email} />
+                                    {errors.Email && touched.Email ? <Text style={globalStyles.ErrorMessages}><ErrorMessage name='Email' /></Text> : <></>}
+                                    <TextInput 
+                                        style={globalStyles.input} placeholder="Date of Birth" 
+                                        onChangeText={handleChange('DOB')}
+                                        onBlur={handleBlur('DOB')}
+                                        value={values.DOB}/>
+                                    {errors.DOB && touched.DOB ? <Text style={globalStyles.ErrorMessages}><ErrorMessage name='DOB' /></Text> : <></>}
+                                    <TextInput 
+                                        style={globalStyles.input} placeholder="Customer CNIC" 
+                                        onChangeText={handleChange('CNIC')}
+                                        onBlur={handleBlur('CNIC')}
+                                        value={values.CNIC}/>
+                                    {errors.CNIC && touched.CNIC ? <Text style={globalStyles.ErrorMessages}><ErrorMessage name='CNIC' /></Text> : <></>}
+                                    <CheckBoxContainer value={values.CreditCustomer} name='CreditCustomer' setValue={setFieldValue} text="Credit Customer" />
+                                </KeyboardAwareScrollView>
+                            </View>
+                            <FlatButton text='Add Customer' onPress={handleSubmit} />
+                        </>
+                    )}
+                </Formik>
             </View>
         </TouchableWithoutFeedback>
     )
