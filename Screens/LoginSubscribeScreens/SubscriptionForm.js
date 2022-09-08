bda4fe09d7d0bf64bcc72be4e36a61bd2767151b
NasirKhatri <45204768+NasirKhatri@users.noreply.github.com>
2022-09-08 21:08:40 +0500

    worked on subscription form

--- Screens/LoginSubscribeScreens/SubscriptionForm.js
+++ Screens/LoginSubscribeScreens/SubscriptionForm.js
@@ -1,5 +1,5 @@
 import * as React from 'react';
-import { View, Text, TextInput, TouchableWithoutFeedback, Keyboard } from 'react-native';
+import { View, Text, TextInput, TouchableWithoutFeedback, Keyboard, Alert } from 'react-native';
 import { KeyboardAwareScrollView } from 'react-native-keyboard-aware-scroll-view'
 import { useMutation } from '@tanstack/react-query';
 
@@ -16,13 +16,13 @@ const SubscriptionSchema = Yup.object().shape({
     Name: Yup.string().min(2, 'Too Short').max(20, 'Too Long').required('Required'),
     Gender: Yup.string(),
     Email: Yup.string().email('Invalid Email').required('Required'),
-    PhoneNo: Yup.string().matches(/[0-9]{4}-[0-9]{7}/, 'Invalid Phone No').required('Required'),
+    PhoneNo: Yup.string().matches(/[0-9]{4}-[0-9]{7}/, 'Invalid Phone No').required('Required').max(12),
     Password: Yup.string().min(6, 'Too Short').matches(/[A-Z]/, 'Must Contain Atleast One Upper Case Letter').matches(/[a-z]/, 'Must Contain Atleast One Lower Case Letter').matches(/[0-9]/, 'Must Contain Atleast One Numeric Value').matches(/[!@#$&]/, 'Must Contain Atlease One Special Character').required('Required'),
     RetypePassword: Yup.string().oneOf([Yup.ref('Password'), null], 'Passwords Must Match').required('Required'),
     Voucher: Yup.string().max(40, 'Too Long').required('Required'),
     Address: Yup.string().min(10, 'Too Short').max(200, 'Too Long'),
     DOB: Yup.string(),
-    CNIC: Yup.string().matches(/[0-9]{5}-[0-9]{7}-[0-9]{1}/, 'Invalid CNIC No'),
+    CNIC: Yup.string().matches(/[0-9]{5}-[0-9]{7}-[0-9]{1}/, 'Invalid CNIC No').max(15),
 
 })
 
@@ -37,9 +37,10 @@ const initialValues = {
     Password: '',
     RetypePassword: '',
     Voucher: '',
+    Package: 'Package 1',
 }
 
-const  userSubscription = async (values, actions) => {
+const userSubscription = async (values) => {
 
     try {
         const result = await axios.post("http://192.168.8.103:3000/subscribe", values);
@@ -49,7 +50,7 @@ const  userSubscription = async (values, actions) => {
         };
     }
 
-    catch(err) {
+    catch (err) {
         return {
             msg: err.response.data.msg,
             status: err.response.status
@@ -62,17 +63,24 @@ const  userSubscription = async (values, actions) => {
 const SubscriptionForm = ({ navigation }) => {
     const [passwordSecure, setPasswordSecure] = React.useState(true);
     const [retypePasswordSecure, setRetypePasswordSecure] = React.useState(true);
-    const mutation = useMutation((values, actions) => userSubscription(values, actions));
-    console.log(mutation);
+    const mutation = useMutation((values) => userSubscription(values));
+
+    const handleSubmit = (values, actions) => {
+        mutation.mutateAsync(values);
+        actions.resetForm();
+    }
 
     return (
         <TouchableWithoutFeedback onPress={() => Keyboard.dismiss()}>
             <View style={globalStyles.body}>
-                <Formik initialValues={initialValues} validationSchema={SubscriptionSchema} onSubmit={(values, actions) => mutation.mutate(values, actions)}>
-                    {({ handleChange, handleBlur, handleSubmit, values, errors, touched }) => (
+                <Formik initialValues={initialValues} validationSchema={SubscriptionSchema} onSubmit={(values, actions) => handleSubmit(values, actions)}>
+                    {({ handleChange, handleBlur, handleSubmit, resetForm, values, errors, touched }) => (
                         <View style={{ flex: 1, justifyContent: 'flex-start' }}>
                             <KeyboardAwareScrollView showsVerticalScrollIndicator={false}>
-                                <Text>Package: Basic</Text>
+                                <Text style={globalStyles.inputLabel}>Package</Text>
+                                <Dropdown value={values.Package} setValue={handleChange('Package')} data={['Package 1', 'Package 2', 'Package 3']} />
+                                {errors.Package && touched.Package ? <Text style={globalStyles.ErrorMessages}><ErrorMessage name='Package' /></Text> : <></>}
+
                                 <Text style={globalStyles.inputLabel}>Name</Text>
                                 <TextInput
                                     style={globalStyles.input} placeholder="Enter full name"
@@ -102,7 +110,7 @@ const SubscriptionForm = ({ navigation }) => {
                                 {errors.PhoneNo && touched.PhoneNo ? <Text style={globalStyles.ErrorMessages}><ErrorMessage name='PhoneNo' /></Text> : <></>}
 
                                 <Text style={globalStyles.inputLabel}>Address</Text>
-                                <TextInput 
+                                <TextInput
                                     style={globalStyles.input} placeholder="User Address"
                                     onChangeText={handleChange('Address')}
                                     onBlur={handleBlur('Address')}
@@ -110,7 +118,7 @@ const SubscriptionForm = ({ navigation }) => {
                                 {errors.Address && touched.Address ? <Text style={globalStyles.ErrorMessages}><ErrorMessage name='Address' /></Text> : <></>}
 
                                 <Text style={globalStyles.inputLabel}>Date of Birth</Text>
-                                <TextInput 
+                                <TextInput
                                     style={globalStyles.input} placeholder="Date of Birth"
                                     onChangeText={handleChange('DOB')}
                                     onBlur={handleBlur('DOB')}
@@ -118,7 +126,7 @@ const SubscriptionForm = ({ navigation }) => {
                                 {errors.DOB && touched.DOB ? <Text style={globalStyles.ErrorMessages}><ErrorMessage name='DOB' /></Text> : <></>}
 
                                 <Text style={globalStyles.inputLabel}>CNIC Number</Text>
-                                <TextInput 
+                                <TextInput
                                     style={globalStyles.input} placeholder="User CNIC"
                                     onChangeText={handleChange('CNIC')}
                                     onBlur={handleBlur('CNIC')}
@@ -136,7 +144,7 @@ const SubscriptionForm = ({ navigation }) => {
                                     {values.Password ? <Text style={{ textAlignVertical: 'center' }} onPress={() => setPasswordSecure(!passwordSecure)}>{passwordSecure ? 'Show' : 'Hide'}</Text> : <></>}
                                 </View>
                                 {errors.Password && touched.Password ? <Text style={globalStyles.ErrorMessages}><ErrorMessage name='Password' /></Text> : <></>}
-                                
+
                                 <Text style={globalStyles.inputLabel}>Retype Password</Text>
                                 <View style={globalStyles.PasswordContainer}>
                                     <TextInput
@@ -148,7 +156,7 @@ const SubscriptionForm = ({ navigation }) => {
                                     {values.RetypePassword ? <Text style={{ textAlignVertical: 'center' }} onPress={() => setRetypePasswordSecure(!retypePasswordSecure)}>{retypePasswordSecure ? 'Show' : 'Hide'}</Text> : <></>}
                                 </View>
                                 {errors.RetypePassword && touched.RetypePassword ? <Text style={globalStyles.ErrorMessages}><ErrorMessage name='RetypePassword' /></Text> : <></>}
-                                
+
                                 <Text style={globalStyles.inputLabel}>Payment Voucher</Text>
                                 <TextInput
                                     style={globalStyles.input} placeholder='Payment Voucher / Transaction No'
@@ -162,6 +170,11 @@ const SubscriptionForm = ({ navigation }) => {
                         </View>
                     )}
                 </Formik>
+                {
+                    mutation.isLoading ? <Text>Loading....</Text> :
+                        mutation.isError ? Alert.alert('Instore Order', mutation.data.msg) :
+                            mutation.isSuccess ? Alert.alert('Instore Order', mutation.data.msg) : null
+                }
                 <LoginSubscribeFooter text1='Already have account?' text2='Login' linkTo='Login' navigation={navigation} />
             </View>
         </TouchableWithoutFeedback>
