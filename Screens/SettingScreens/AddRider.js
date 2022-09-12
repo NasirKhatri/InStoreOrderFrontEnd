b133f77b4b361937f345bcdec64ebd9e90bac122
NasirKhatri <45204768+NasirKhatri@users.noreply.github.com>
2022-09-12 21:56:32 +0500

    Branches dropdown fixed in Addrider screen

--- Screens/SettingScreens/AddRider.js
+++ Screens/SettingScreens/AddRider.js
@@ -10,11 +10,15 @@ import { Formik, ErrorMessage } from 'formik';
 import * as Yup from "yup";
 
 import globalStyles from '../../globalStyles'
+import axios from 'axios';
+import { BaseUrl } from '../../SharedFunctions.js/StoreContext';
+import { useQuery } from '@tanstack/react-query';
+import { getData } from '../../SharedFunctions.js/SetGetData';
 
 const initialValues = {
     Name: '',
     LicenseNumber: '',
-    Branch: 'Branch A',
+    Branch: '0',
     ContactNumber: '',
     Active: true,
 }
@@ -26,6 +30,19 @@ const UserSchema = Yup.object().shape({
     ContactNumber: Yup.string().matches(/[0-9]{4}-[0-9]{7}/, 'Invalid Phone No').required('Required'),
 })
 
+const getBranches = async () => {
+    const user = await getData('user');
+    const token = user.Token;
+    const url = `${BaseUrl}/branches/${user.ClientID}`;
+    const data = await axios.get(url, {
+        headers: {
+            authorization: `Bearer ${token}`,
+
+        }
+    });
+    return data.data;
+}
+
 const addRiderRequest = (values, actions) => {
     console.log(values);
     actions.resetForm();
@@ -33,48 +50,60 @@ const addRiderRequest = (values, actions) => {
 }
 
 export const AddRider = () => {
-    return (
-        <TouchableWithoutFeedback onPress={() => Keyboard.dismiss()}>
-            <View style={globalStyles.body}>
-                <Formik initialValues={initialValues} validationSchema={UserSchema} onSubmit={(values, actions) => addRiderRequest(values, actions)}>
-                    {({ handleChange, handleBlur, handleSubmit, setFieldValue, values, errors, touched }) => (
-                        <>
-                            <KeyboardAwareScrollView>
-                                <Text style={globalStyles.inputLabel}>Name *</Text>
-                                <TextInput 
-                                    style={globalStyles.input} placeholder="Rider Name"
-                                    onChangeText={handleChange('Name')}
-                                    onBlur={handleBlur('Name')}
-                                    value={values.Name}  />
-                                {errors.Name && touched.Name ? <Text style={globalStyles.ErrorMessages}><ErrorMessage name='Name' /></Text> : <></>}
+    const { isLoading, data } = useQuery(['branches'], getBranches);
+    let branches = [];
+    if (!isLoading) {
+        initialValues.Branch = (data[0].BranchID).toString();
+        data.forEach(element => {
+            branches.push({
+                name: element.BranchName,
+                id: `${element.BranchID}`
+            });
+        });
 
-                                <Text style={globalStyles.inputLabel}>License Number *</Text>
-                                <TextInput 
-                                    style={globalStyles.input} placeholder="License Number"
-                                    onChangeText={handleChange('LicenseNumber')}
-                                    onBlur={handleBlur('LicenseNumber')}
-                                    value={values.LicenseNumber}  />
-                                {errors.LicenseNumber && touched.LicenseNumber ? <Text style={globalStyles.ErrorMessages}><ErrorMessage name='LicenseNumber' /></Text> : <></>}
+        return (
+            <TouchableWithoutFeedback onPress={() => Keyboard.dismiss()}>
+                <View style={globalStyles.body}>
+                    <Formik initialValues={initialValues} validationSchema={UserSchema} onSubmit={(values, actions) => addRiderRequest(values, actions)}>
+                        {({ handleChange, handleBlur, handleSubmit, setFieldValue, values, errors, touched }) => (
+                            <>
+                                <KeyboardAwareScrollView>
+                                    <Text style={globalStyles.inputLabel}>Name *</Text>
+                                    <TextInput
+                                        style={globalStyles.input} placeholder="Rider Name"
+                                        onChangeText={handleChange('Name')}
+                                        onBlur={handleBlur('Name')}
+                                        value={values.Name} />
+                                    {errors.Name && touched.Name ? <Text style={globalStyles.ErrorMessages}><ErrorMessage name='Name' /></Text> : <></>}
 
-                                <Text style={globalStyles.inputLabel}>Contact Number *</Text>
-                                <TextInput 
-                                    keyboardType='numeric' style={globalStyles.input} placeholder="Phone Number (i.e. 0123-1234567)"
-                                    onChangeText={handleChange('ContactNumber')}
-                                    onBlur={handleBlur('ContactNumber')}
-                                    value={values.ContactNumber}  />
-                                {errors.ContactNumber && touched.ContactNumber ? <Text style={globalStyles.ErrorMessages}><ErrorMessage name='ContactNumber' /></Text> : <></>}
+                                    <Text style={globalStyles.inputLabel}>License Number *</Text>
+                                    <TextInput
+                                        style={globalStyles.input} placeholder="License Number"
+                                        onChangeText={handleChange('LicenseNumber')}
+                                        onBlur={handleBlur('LicenseNumber')}
+                                        value={values.LicenseNumber} />
+                                    {errors.LicenseNumber && touched.LicenseNumber ? <Text style={globalStyles.ErrorMessages}><ErrorMessage name='LicenseNumber' /></Text> : <></>}
 
-                                <Text style={globalStyles.inputLabel}>Branch *</Text>
-                                <Dropdown value={values.Branch} setValue={handleChange('Branch')} data={['Branch A', 'Branch B']} />
-                                {errors.Branch && touched.Branch ? <Text style={globalStyles.ErrorMessages}><ErrorMessage name='Branch' /></Text> : <></>}
+                                    <Text style={globalStyles.inputLabel}>Contact Number *</Text>
+                                    <TextInput
+                                        keyboardType='numeric' style={globalStyles.input} placeholder="Phone Number (i.e. 0123-1234567)"
+                                        onChangeText={handleChange('ContactNumber')}
+                                        onBlur={handleBlur('ContactNumber')}
+                                        value={values.ContactNumber} />
+                                    {errors.ContactNumber && touched.ContactNumber ? <Text style={globalStyles.ErrorMessages}><ErrorMessage name='ContactNumber' /></Text> : <></>}
 
-                                <CheckBoxContainer value={values.Active} name="Active" setValue={setFieldValue} text="Active" />
-                            </KeyboardAwareScrollView>
-                            <FlatButton text='Add Rider' onPress={handleSubmit} />
-                        </>
-                    )}
-                </Formik>
-            </View>
-        </TouchableWithoutFeedback>
-    )
+                                    <Text style={globalStyles.inputLabel}>Branch *</Text>
+                                    <Dropdown value={values.Branch} setValue={handleChange('Branch')} data={branches} />
+                                    {errors.Branch && touched.Branch ? <Text style={globalStyles.ErrorMessages}><ErrorMessage name='Branch' /></Text> : <></>}
+
+                                    <CheckBoxContainer value={values.Active} name="Active" setValue={setFieldValue} text="Active" />
+                                </KeyboardAwareScrollView>
+                                <FlatButton text='Add Rider' onPress={handleSubmit} />
+                            </>
+                        )}
+                    </Formik>
+                </View>
+            </TouchableWithoutFeedback>
+        )
+    }
 }
